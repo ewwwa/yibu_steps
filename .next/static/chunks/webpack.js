31618e12282a828cf9876d3dacdfe88b021bd7ff
Sundesan <eva.anguita@rebelslab.com>
2025-01-22 17:01:19 +0100

    Backup for next.config.mjs issues

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "638cf410b8fd45bd"; }
+/******/ 		__webpack_require__.h = function() { return "bfb91e8ba258b953"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
